a83243611ae7209673a7fce851f3b21ac970c47a
Kksks932 <krushnaverma1@gmail.com>
2025-09-18 11:58:59 +0530

    hello

---
+++ firstprogram.py
@@ -0,0 +1,19 @@
+name = "gandu "
+age= 19.6
+price=200
+a=4
+b=2
+print(type(a/b))
+sum= a+b
+gandu = None  
+print("my name is :", name )
+print ("my age is :",age)
+print(type(name))
+print(type(age))
+print(type(price) )
+print((type(gandu))
+      )   
+print(28+53)
+print(a+b)
+print(sum)
+print(a+b)
---
+++ k.py
@@ -0,0 +1,19 @@
+print("hello world"
+      )
+name = "something"
+age=45
+price =45.7
+a=4
+b=2
+print(a+b)
+print(a/b)
+print(type(a))
+print(type(a/b))
+print(type(age))
+print(type(name))
+print(type(price))
+print(a==b)#f
+print(a!=b)#t
+print(a>=b) #t
+print(a<=b)#false
+
